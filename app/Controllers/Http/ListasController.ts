5b41307841f7b838285b2c9941e273a4ca05f433
LeoAboard <leonardoarmelin2003@outlook.com>
2025-08-08 15:44:38 -0400

    Finalizado CRUD de lista

--- app/Controllers/Http/ListasController.ts
+++ app/Controllers/Http/ListasController.ts
@@ -1,20 +1,55 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Lista from 'App/Models/Lista'
 
 export default class ListasController {
 
-    public async create(){
+    public async create({ request, payload }: HttpContextContract){
+        const { mensagem } = request.all()
 
+        try{
+            await Lista.create({
+                id_usuario: payload,
+                mensagem,
+                status: false
+            })
+
+            return `Sua mensagem foi enviada: ${mensagem}`
+
+        } catch(error:any){
+            return 'Escreva uma mensagem.'
+        }
     }
 
-    public async exibir(){
+    public async exibir({ payload }: HttpContextContract){
+        const lista = await Lista.query().where('id_usuario', '=', `${payload}`).select('id', 'mensagem', 'status')
 
+        return lista
     }
 
-    public async atualizar(){
+    public async atualizar({ request, payload }: HttpContextContract){
+        const { id } = request.all()
 
+        try{
+            const lista = await Lista.query().where('id', id).andWhere('id_usuario', payload).firstOrFail()
+
+            lista.merge({ status: !lista.status })
+            await lista.save()
+            return `O status de sua tarefa foi alterado:\nMensagem: ${lista.mensagem}\nStatus: ${lista.status ? 'Concluído' : 'Pendente'}`
+
+        } catch(error:any){
+            return 'Você não pode alterar essa task.'
+        }
     }
 
-    public async excluir(){
+    public async excluir({ request, payload }: HttpContextContract){
+        const { id } = request.all()
 
+        try{
+            const lista = await Lista.query().where('id', id).andWhere('id_usuario', payload).delete()
+            if(!lista[0]) throw console.error();
+            return 'Task apagada com sucesso!'
+        } catch(error:any){
+            return 'Você não pode apagar essa task.'
+        }
     }
 }
